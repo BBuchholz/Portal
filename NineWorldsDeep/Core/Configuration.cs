f5f6d3ff15c9b7521819d0460112abfbdfc61568
Brent Buchholz <brentbuchholz@gmail.com>
2015-11-11 11:54:25 -0600

    configuration grew

---
+++ NineWorldsDeep/Core/Configuration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineWorldsDeep.Core
+{
+    public class Configuration
+    {
+        //TODO: hard-coded values need to be transformed into config files and defaults
+
+        public static string AbletonProjectsFolder { get { return @"C:\NWD-AUX\abletonProjects"; } }
+        public static string AudacityProjectsFolder { get { return @"C:\NWD-AUX\audacityProjects"; } }
+        public static string CubaseProjectsFolder { get { return @"C:\NWD-AUX\cubaseProjects"; } }
+        public static string FLStudioProjectsFolder { get { return @"C:\NWD-AUX\flStudioProjects"; } }
+
+        public static string MySqlProjectsFolder
+        {
+            get
+            {
+                return @"C:\NWD\GERM\code\sql\";
+            }
+        }
+
+        public static string VisualStudioProjectsFolder
+        {
+            get
+            {
+                return @"C:\Users\Brent\Documents\Visual Studio 2015\Projects";
+            }
+        }
+    }
+}
