8093c9f782bdbe2633b8fdd33d23ca8f32422e3d
Brent Buchholz <brentbuchholz@gmail.com>
2016-03-15 01:00:01 -0500

    added file (didn't auto add for some reason)

---
+++ NineWorldsDeep/Db/PathTagLink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NineWorldsDeep.Db
+{
+    public class PathTagLink
+    {
+        public string PathValue { get; set; }
+        public string TagValue { get; set; }
+    }
+}
