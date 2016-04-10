73ab8d7b0dff92b3a5a9b6ed6aad750f1c8a75fb
Brent Buchholz <brentbuchholz@gmail.com>
2016-04-10 15:41:18 -0500

    added stub for handling android path roots (came up in NwdWarehouse planning, will be part of the overall scheme eventually)

--- NineWorldsDeep/Parser/NwdUriConverter.cs
+++ NineWorldsDeep/Parser/NwdUriConverter.cs
@@ -8,6 +8,37 @@ namespace NineWorldsDeep.Parser
 {
     public class NwdUriConverter
     {
+        /// <summary>
+        /// for a list of Android paths (such as those from 
+        /// FileHashIndex or TagFileIndex in Gauntlet), finds all of 
+        /// the roots from all entries containing "NWD".
+        /// the first instance of "NWD" is where the break
+        /// occurs, so "/storage/0/NWD-SNDBX/test1/NWD/..."
+        /// would store "/storage/0" not 
+        /// "/storage/0/NWD-SNDBX/test1/..."
+        /// </summary>
+        /// <param name="paths"></param>
+        /// <returns></returns>
+        public static List<string> GetAndroidPathRoots(IEnumerable<string> paths)
+        {
+            //obviously, from the name, intended for Android Path handling
+
+            //TODO: paths refactor, add to NWD.NwdUriSomething
+            //TO GET ALL ROOTS (may not need this here, this random thought was aiming for path based, not hash based. If it isn't needed, add these comments as comments for a Utility method somewhere, with a TODO task to implement included :)
+            //go through paths of both files,
+            //any path that contains "NWD",
+            //find the first instance,
+            //and store the preceding part as one
+            //root path. This should find external and internal.
+            //then, go back through list, trimming
+            //all instances all root paths
+            //and get those paths that do not contain "NWD"
+            //(just by trimming, I'm not referring to a Contains() test)
+            //this will get folders like Pictures/Screenshots and DCIM/camera
+
+            throw new NotImplementedException();
+        }
+
         public static string NwdUriToFileSystemPath(string uri)
         {
             Stack<string> nodeStack = UriToNodeNameStack(uri);
