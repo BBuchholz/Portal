be79665cbc6acc9909d5924eff88ab385ecc6c84
Brent Buchholz <brentbuchholz@gmail.com>
2016-11-10 20:03:59 -0600

    created controller to hold common logic for both tagger grids

--- NineWorldsDeep/Tagger/ITaggerGrid.cs
+++ NineWorldsDeep/Tagger/ITaggerGrid.cs
@@ -4,10 +4,21 @@ namespace NineWorldsDeep.Tagger
 {
     public interface ITaggerGrid
     {
+        bool HasPendingChanges { get; }
+        FileElement SelectedFileElement { get; }
+        IEnumerable<FileElement> FileElements { get; }
+
         List<string> GetTagsForCurrentSelection();
         void SetFolderLoadStrategy(IFolderLoadStrategy fls);
         void AppendTagsToCurrentTagStringAndUpdate(List<string> extractions);
         void AppendTagToCurrentTagStringAndUpdate(string v);
         TagMatrix GetTagMatrix();
+        void AddSelectionChangedListener(FileElementActionSubscriber feas);
+        void SaveToFile(string saveFilePath);
+        void AddFolder(string folderPath);
+        void LoadFromFile(string loadFilePath);
+        void SaveToDb();
+        void LoadFromDb(string filePathTopFolder);
+        void Clear();
     }
 }
---
+++ NineWorldsDeep/Tagger/V2/TaggerGridController.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NineWorldsDeep.Tagger.V2
+{
+    public class TaggerGridController : ITaggerGrid
+    {
+        private ListView lvFileElements;
+        private ListView lvTags;
+        private TextBox txtTags;
+        private TextBox txtFilter;
+        private TextBlock tbFileCount;
+        private TextBlock tbStatus;
+
+        private bool pendingChanges = false;
+        private string lastLoadedPath;
+        private TagMatrix tagMatrix = new TagMatrix();
+        private List<FileElementActionSubscriber> selectionChangedListeners =
+            new List<FileElementActionSubscriber>();
+
+        public TaggerGridController(
+            TextBox tagString,
+            TextBox tagFilter,
+            ListView tags, 
+            ListView fileElements, 
+            TextBlock fileCount, 
+            TextBlock status)
+        {
+            txtTags = tagString;
+            txtFilter = tagFilter;
+            lvTags = tags;
+            lvFileElements = fileElements;
+            tbFileCount = fileCount;
+            tbStatus = status;
+        }
+
+        public IEnumerable<FileElement> FileElements
+        {
+            get
+            {
+                return lvFileElements.Items.Cast<FileElement>().ToList();
+            }
+        }
+
+        public bool HasPendingChanges
+        {
+            get
+            {
+                return pendingChanges;
+            }
+        }
+
+        public FileElement SelectedFileElement
+        {
+            get
+            {
+                return (FileElement)lvFileElements.SelectedItem;
+            }
+        }
+
+        public void AddFolder(string folderPath)
+        {
+            tagMatrix.AddFolder(folderPath);
+
+            LoadFileElementList(tagMatrix.GetFilePaths());
+        }
+
+        private void LoadFileElementList(IEnumerable<string> pathList)
+        {
+            lvFileElements.ItemsSource = ToFileElementList(pathList);
+            tbFileCount.Text = "Count: " + pathList.Count();
+            if (pathList.Count() > 0)
+            {
+                lvFileElements.SelectedIndex = 0;
+            }
+        }
+
+        private List<FileElement> ToFileElementList(IEnumerable<string> pathList)
+        {
+            List<FileElement> feLst = new List<FileElement>();
+
+            foreach (string file in pathList)
+            {
+                feLst.Add(FileElement.FromPath(file, tagMatrix));
+            }
+
+            feLst.Sort((fe1, fe2) => fe1.Name.CompareTo(fe2.Name));
+
+            return feLst;
+        }
+
+        public void AddSelectionChangedListener(FileElementActionSubscriber feas)
+        {
+            selectionChangedListeners.Add(feas);
+        }
+
+        public void AppendTagsToCurrentTagStringAndUpdate(List<string> tags)
+        {
+            foreach (string tag in tags)
+            {
+                AppendTagToCurrentTagString(tag);
+            }
+
+            Update();
+        }
+
+        public void AppendTagToCurrentTagString(string tag)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTags.Text))
+            {
+                tag = ", " + tag;
+            }
+
+            txtTags.Text = txtTags.Text + tag;
+        }
+
+        public void Update()
+        {
+            tbStatus.Text = "Updating...";
+
+            FileElement fe = (FileElement)lvFileElements.SelectedItem;
+
+            if (fe != null)
+            {
+                tagMatrix.UpdateTagString(fe, txtTags.Text);
+
+                PopulateTagListView();
+
+                tbStatus.Text = "Updated.";
+                SetPendingChanges(true);
+            }
+        }
+
+        private void SetPendingChanges(bool changesPending)
+        {
+            pendingChanges = changesPending;
+            if (!pendingChanges)
+            {
+                tbStatus.Text = "";
+            }
+            else
+            {
+                tbStatus.Text = "Pending Changes";
+            }
+        }
+
+        private void PopulateTagListView()
+        {
+            lvTags.ItemsSource = tagMatrix.GetTagModelItems(txtFilter.Text);
+        }
+        
+        public void AppendTagToCurrentTagStringAndUpdate(string tag)
+        {
+            AppendTagToCurrentTagString(tag);
+
+            Update();
+        }
+
+        public void Clear()
+        {
+            tagMatrix.Clear();
+            lvFileElements.ItemsSource = null;
+            lvTags.ItemsSource = null;
+        }
+
+        public TagMatrix GetTagMatrix()
+        {
+            return tagMatrix;
+        }
+
+        public List<string> GetTagsForCurrentSelection()
+        {
+            return TagString.Parse(txtTags.Text);
+        }
+
+        public void LoadFromDb(string filePathTopFolder)
+        {
+            tbStatus.Text = "Loading...";
+
+            tagMatrix.LoadFromDb(filePathTopFolder);
+
+            PopulateTagListView();
+
+            tbStatus.Text = "Loaded.";
+            lastLoadedPath = filePathTopFolder;
+            SetPendingChanges(false);
+        }
+
+        public void LoadFromFile(string loadFilePath)
+        {
+            tbStatus.Text = "Loading...";
+
+            tagMatrix.LoadFromXml(loadFilePath);
+
+            PopulateTagListView();
+
+            tbStatus.Text = "Loaded.";
+            SetPendingChanges(false);
+        }
+
+        public void SaveToDb()
+        {
+            tbStatus.Text = "Saving...";
+
+            tagMatrix.SaveToDb();
+
+            tbStatus.Text = "Saved.";
+            SetPendingChanges(false);
+        }
+
+        public void SaveToFile(string saveFilePath)
+        {
+            tbStatus.Text = "Saving...";
+
+            tagMatrix.SaveToXml(saveFilePath);
+
+            tbStatus.Text = "Saved.";
+            SetPendingChanges(false);
+        }
+
+        public void SetFolderLoadStrategy(IFolderLoadStrategy fls)
+        {
+            tagMatrix.SetFolderLoadStrategy(fls);
+        }
+    }
+}
